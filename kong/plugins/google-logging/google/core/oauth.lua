2bf73612db82c161b2aeab28a4dc551a4247d961
Clemens Zeidler <clemens.zeidler@smartparking.com>
2020-09-17 11:06:24 +1200

    Return early and param doc

--- kong/plugins/google-logging/google/core/oauth.lua
+++ kong/plugins/google-logging/google/core/oauth.lua
@@ -8,7 +8,6 @@ local digest = require "resty.openssl.digest"
 
 local _M = {}
 
-
 function _M:GetAccessToken()
   return self.generate_access_token(self)
 end
@@ -52,62 +51,66 @@ function params_builder(tbl)
   return table.concat(tuples, "&")
 end
 
+-- @param scope (string) can be multiple scopes seperated by space, e.g. "scope1 scope2 scope2"
 function construct(self, key_path, key, scope)
   local self
   local key_table = key_path and read_key_return_json(key_path) or key
-  if key_table then
-    self = setmetatable({
-      _client_email = key_table.client_email,
-      _private_key = key_table.private_key,
-      _project_id = key_table.project_id,
-      _access_token = nil,
-      _access_token_expire_time = nil,
-      _scope = scope,
-      _auth_token_url = key_table.token_uri,
-      generate_access_token = function(self)
-        if not self._access_token or os.time() > self._access_token_expire_time - 60 then
-          self.refresh_access_token(self)
-        end
-        return self._access_token
-      end,
-      refresh_access_token = function(self)
-        local time = os.time()
-        local jwt = self.make_jwt(self)
-        local req_params = params_builder({
-         grant_type = "urn:ietf:params:oauth:grant-type:jwt-bearer",
-         assertion = jwt
-        })
-
-        local res = assert(http.request(self._auth_token_url, req_params))
-        res = json.decode(res)
-
-        if res.error then
-         error("Failed auth: " .. tostring(res.error))
-        end
-        self._access_token_expire_time = time + res.expires_in
-        self._access_token = res.access_token
-      end,
-      make_jwt = function(self)
-        local claims = json.encode({
-         iss = self._client_email,
-         aud = self._auth_token_url,
-         scope = self._scope,
-         iat = os.time(),
-         exp = os.time() + (60 * 60)
-        })
-        local sign_input = mime.b64('{"alg":"RS256","typ":"JWT"}') .. "." .. mime.b64(claims)
-        local signature = self.sign_string(self, sign_input)
-        return sign_input .. "." .. signature
-      end,
-      sign_string = function(self, string_to_sign)
-        local key = assert(pkey.new(self._private_key, { format = "PEM", type = "pr" }))
-        local d = assert(digest.new("sha256WithRSAEncryption"))
-        d:update(string_to_sign)
-        local sig = assert(key:sign(d))
-        return (mime.b64(sig))
-      end
-    }, {__index = _M})
+  if key_table == nil then
+    error("Either key_path or key must be specified")
+    return self
   end
+
+  self = setmetatable({
+    _client_email = key_table.client_email,
+    _private_key = key_table.private_key,
+    _project_id = key_table.project_id,
+    _access_token = nil,
+    _access_token_expire_time = nil,
+    _scope = scope,
+    _auth_token_url = key_table.token_uri,
+    generate_access_token = function(self)
+      if not self._access_token or os.time() > self._access_token_expire_time - 60 then
+        self.refresh_access_token(self)
+      end
+      return self._access_token
+    end,
+    refresh_access_token = function(self)
+      local time = os.time()
+      local jwt = self.make_jwt(self)
+      local req_params = params_builder({
+       grant_type = "urn:ietf:params:oauth:grant-type:jwt-bearer",
+       assertion = jwt
+      })
+
+      local res = assert(http.request(self._auth_token_url, req_params))
+      res = json.decode(res)
+
+      if res.error then
+       error("Failed auth: " .. tostring(res.error))
+      end
+      self._access_token_expire_time = time + res.expires_in
+      self._access_token = res.access_token
+    end,
+    make_jwt = function(self)
+      local claims = json.encode({
+       iss = self._client_email,
+       aud = self._auth_token_url,
+       scope = self._scope,
+       iat = os.time(),
+       exp = os.time() + (60 * 60)
+      })
+      local sign_input = mime.b64('{"alg":"RS256","typ":"JWT"}') .. "." .. mime.b64(claims)
+      local signature = self.sign_string(self, sign_input)
+      return sign_input .. "." .. signature
+    end,
+    sign_string = function(self, string_to_sign)
+      local key = assert(pkey.new(self._private_key, { format = "PEM", type = "pr" }))
+      local d = assert(digest.new("sha256WithRSAEncryption"))
+      d:update(string_to_sign)
+      local sig = assert(key:sign(d))
+      return (mime.b64(sig))
+    end
+  }, {__index = _M})
   return self
 end
 
